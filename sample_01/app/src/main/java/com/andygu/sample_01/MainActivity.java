8b6161d62aaa5747320e51401c1c49680c53e8ed
DESKTOP-GIL4A5G\UnidynaPC <andygu@unidyna.com>
2018-06-25 11:58:04 +0800

    1.Activity生命週期測試

--- sample_01/app/src/main/java/com/andygu/sample_01/MainActivity.java
+++ sample_01/app/src/main/java/com/andygu/sample_01/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         new AlertDialog.Builder(MainActivity.this).setTitle("BMI說明").setMessage("體重(kg)/身高的平方(m)").setNegativeButton("OK",null).show();
       }
     });
+    Toast.makeText(this,"onCreate",Toast.LENGTH_LONG).show();
   }
 
   private void findViews() {
@@ -62,4 +63,33 @@ public class MainActivity extends AppCompatActivity {
     //    .show();
   }
 
+  @Override protected void onStart() {
+    super.onStart();
+    Toast.makeText(this,"onStart",Toast.LENGTH_LONG).show();
+  }
+
+  @Override protected void onStop() {
+    super.onStop();
+    Toast.makeText(this,"onStop",Toast.LENGTH_LONG).show();
+  }
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+    Toast.makeText(this,"onDestroy",Toast.LENGTH_LONG).show();
+  }
+
+  @Override protected void onPause() {
+    super.onPause();
+    Toast.makeText(this,"onPause",Toast.LENGTH_LONG).show();
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+    Toast.makeText(this,"onResume",Toast.LENGTH_LONG).show();
+  }
+
+  @Override protected void onRestart() {
+    super.onRestart();
+    Toast.makeText(this,"onRestart",Toast.LENGTH_LONG).show();
+  }
 }
