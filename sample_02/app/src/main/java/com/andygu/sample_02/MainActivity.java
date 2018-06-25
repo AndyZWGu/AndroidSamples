68df72946806c11962f8527f1762cb1143ad6dca
DESKTOP-GIL4A5G\UnidynaPC <andygu@unidyna.com>
2018-06-25 15:25:54 +0800

    修正登入返回按鈕的bug

--- sample_02/app/src/main/java/com/andygu/sample_02/MainActivity.java
+++ sample_02/app/src/main/java/com/andygu/sample_02/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+  public static final int RC_LOGIN = 1; //代表該功能的常數
   private boolean isLogin = false;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
     //沒登入時返回登入畫面
     if(!isLogin){
       Intent intent = new Intent(this,LoginActivity.class);
-      startActivity(intent);
+      startActivityForResult(intent,RC_LOGIN);
+      //startActivity(intent); //會有返回鍵無視登入的Bug,故用上面方法轉換頁面才對
     }
 
   }
